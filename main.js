4654e1ecdaba07c49b66f07368b0973f9a834a31
Madinaxon Toxirjonovna <madinaxontoxirjonovna@gamil.com>
2024-03-01 16:19:29 +0500

    Lesson 4oy (1)

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict'
+// bu cssda ishlatadigan * o'rniga
+
+let String = prompt('String typedagi malumot')
+
+let Number = prompt('Number typedagi malumot kiriting')
+
+let Boolen = prompt("Boolen typedagi malumot kiritng")
+
+confirm("Hamma malumotlarni to'g'ri kiritganingizga ishonchingiz komilmi?")
+
+alert('Siz kiritgan ' + String + ' sozi string typega tegishli\n' + "Siz kiritgan " + Number + ' sozi number typega tegishli\n' + "Siz kiritgan" +Boolen+' sozi boolen typega tegishli')
+
+// alert("\n String typedagi malumot = " + String + "  \n Number typedagi malumot kiriting = " + Number + "  \n Boolen typedagi malumot kiritng = " + Boolean+ " \n ")
+
